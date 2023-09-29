b4aafff840e3c67b207560060a5a4d32524d207a
banhdung <lmpdungbd@gmail.com>
2023-09-29 21:14:40 +0700

    commit header

---
+++ e-comerce-antd/src/Components/Header/index.js
@@ -0,0 +1,74 @@
+import App from "../../App";
+import { Menu } from "antd";
+import { HomeOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
+
+export default function AppHeader() {
+  const navigate = useNavigate();
+
+  const onMenuClick = (item) => {
+    navigate(`/${item.key}`);
+  };
+  return (
+    <div className="appHeader">
+      <Menu
+        onClick={onMenuClick}
+        mode="horizontal"
+        items={[
+          {
+            label: <HomeOutlined />,
+            key: "",
+          },
+          {
+            label: "Men",
+            key: "men",
+            children: [
+              {
+                label: "Men's Shirts",
+                key: "mens-shirts",
+              },
+              {
+                label: "Men's Shoes",
+                key: "mens-shoes",
+              },
+              {
+                label: "Men's Watches",
+                key: "mens-watches",
+              },
+            ],
+          },
+          {
+            label: "Women",
+            key: "women",
+            children: [
+              {
+                label: "Women's Dresses",
+                key: "womens-dresses",
+              },
+              {
+                label: "Women's Shoes",
+                key: "womens-shoes",
+              },
+              {
+                label: "Women's Watches",
+                key: "womens-watches",
+              },
+              {
+                label: "Women's Bags",
+                key: "womens-bags",
+              },
+              {
+                label: "Women's Jewellery",
+                key: "womens-jewellery",
+              },
+            ],
+          },
+          {
+            label: "Accessories",
+            key: "accessories",
+          },
+        ]}
+      ></Menu>
+    </div>
+  );
+}
